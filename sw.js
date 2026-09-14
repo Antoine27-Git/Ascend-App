8eeaad93c128dbe979d44bb05fa7a9a92b23d72f
Antoine27-Git <silvantoine2791@gmail.com>
2026-09-14 23:55:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='ascend-v2';
+const C='ascend-v3';
 const ASSETS=['./','./index.html','./manifest.json','./favicon.png','./apple-touch-icon.png','./icon-192x192.png','./icon-512x512.png','./icon-maskable-192.png','./icon-maskable-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
